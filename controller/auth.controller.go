eca246e4b7ba5c326fe863e7ecc8382830982454
Your Name <samsulr99@gmail.com>
2022-07-27 16:22:05 +0700

    add generate jwt token

--- controller/auth.controller.go
+++ controller/auth.controller.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"log"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/samsul-rijal/go-api/config/database"
@@ -42,8 +46,23 @@ func LoginController(ctx *fiber.Ctx) error {
 		})
 	}
 
+	//generate token
+	claims := jwt.MapClaims{}
+	claims["name"] = user.Name
+	claims["email"] = user.Email
+	claims["address"] = user.Address
+	claims["exp"] = time.Now().Add(time.Minute * 2).Unix() // 2 menit expired
+
+	token, errGenerateToken := utils.GenerateToken(&claims)
+	if errGenerateToken != nil{
+		log.Println(errGenerateToken)
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message":"unauthorized",
+		})
+	}
+
 	return ctx.JSON(fiber.Map{
-		"token": "secret",
+		"token": token,
 	})
 
 }
---
+++ utils/jwt.go
@@ -0,0 +1,15 @@
+package utils
+
+import "github.com/dgrijalva/jwt-go"
+
+var SecretKey = "SECRET_KEY"
+
+func GenerateToken(claims *jwt.MapClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	webtoken, err := token.SignedString([]byte(SecretKey))
+	if err != nil{
+		return "", err
+	}
+
+	return webtoken, nil
+}
